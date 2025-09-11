21fcfb01b94d0657c2078deaffa2abaa38095b37
NeoVentureStudio <neoventurestudio25@gmail.com>
2025-09-11 02:16:41 -0700

    Add files via upload

---
+++ NewsPage/News.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const cards = document.querySelectorAll(".news-card");
+const bgMusic = document.getElementById("bgMusic");
+const soundBtn = document.getElementById("sound-toggle");
+
+soundBtn.addEventListener("click", () => {
+  if (bgMusic.paused) {
+    bgMusic.play();
+    soundBtn.textContent = "🔊";
+  } else {
+    bgMusic.pause();
+    soundBtn.textContent = "🔇";
+  }
+})
+
+  cards.forEach((card, i) => {
+    card.style.opacity = 0;
+    card.style.transform = "translateY(5vh)";
+    setTimeout(() => {
+      card.style.transition = "0.8s ease";
+      card.style.opacity = 1;
+      card.style.transform = "translateY(0)";
+    }, i * 300);
+  });
+});
